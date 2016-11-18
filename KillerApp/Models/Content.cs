e6c56ce1200404cb559ef793b63f44b70dd5d2cf
Lombarts,Arjan A.J.P.F <a.lombarts@student.fontys.nl>
2016-11-18 13:12:14 +0100

    MVC opgezet

---
+++ KillerApp/Models/Content.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KillerApp.Models
+{
+    public abstract class Content
+    {
+        public TimeSpan Duur { get; set; }
+    }
+}
